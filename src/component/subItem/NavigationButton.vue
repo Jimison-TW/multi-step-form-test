c6a586c40e6ee3e6d9dfea88090ab5d049f46993
fish <>
2025-11-09 03:24:34 +0800

    新增navigation按鈕功能，首頁隱藏back按鈕，末頁顯示confirm按鈕

--- src/component/subItem/NavigationButton.vue
+++ src/component/subItem/NavigationButton.vue
@@ -1,14 +1,21 @@
 <template>
     <div class="button-container">
-        <button class="back" @click="$emit('prev')">Go Back</button>
-        <button class="next" @click="$emit('next')">Next Step</button>
+        <button class="back" :class="{ invisible: currentStep === 1 }" @click="$emit('prev')">Go
+            Back</button>
+        <button v-if="currentStep < 4" class="next" @click="$emit('next')">Next Step</button>
+        <button v-if="currentStep === 4" class="confirm" @click="$emit('submit')">
+            Confirm
+        </button>
+
     </div>
 </template>
 
 <script setup lang="ts">
+defineProps<{ currentStep: number }>();
 const emit = defineEmits<{
     (e: 'next'): void;
     (e: 'prev'): void;
+    (e: 'submit'): void;
 }>();
 </script>
 
@@ -26,8 +33,19 @@ const emit = defineEmits<{
     color: $grey-500;
 }
 
+.invisible {
+    visibility: hidden;
+}
+
 .next {
     background-color: $blue-950;
+    width: 120px;
+    height: 45px;
+    border-radius: 8px;
+}
+
+.confirm {
+    background-color: $purple-600;
     width: 120px;
     height: 45px;
     border-radius: 8px;
--- src/views/PopupView.vue
+++ src/views/PopupView.vue
@@ -9,7 +9,7 @@
             <AddOns v-show="currentStep === 3" />
             <FinishingUp v-show="currentStep === 4" />
             <div class="navigation-wrapper">
-                <NavigationButton @next="nextStep" @prev="prevStep" />
+                <NavigationButton :current-step="currentStep" @next="nextStep" @prev="prevStep" />
             </div>
         </div>
     </div>
